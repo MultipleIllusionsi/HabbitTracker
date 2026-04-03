65860e7db9fdce0af29c1a9892279502349bce15
Sergey Zakharov <zakharov.nsg@gmail.com>
2026-04-03 13:50:39 +0500

    Initial Commit

---
+++ HabbitTracker/HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitTrackerApp.swift
+//  HabbitTracker
+//
+//  Created by Сергей Захаров on 03.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
